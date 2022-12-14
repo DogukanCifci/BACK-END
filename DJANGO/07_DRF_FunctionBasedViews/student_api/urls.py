d394c7ad0fe41b9b93438607b76889abaddd8ac8
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-12-14 11:35:19 +0100

    detail added

--- DJANGO/07_DRF_FunctionBasedViews/student_api/urls.py
+++ DJANGO/07_DRF_FunctionBasedViews/student_api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home, student_list
+from .views import home, student_list,students_create,student_detail
 
 #after /api :
 urlpatterns = [
   path("", home),
   path("student_list/", student_list),
+  path("student_create/", students_create),
+  path("student_detail/<int:pk>", student_detail) #student_Detail fonksiyonunda pk diye bir parametre kabul ettik onu burada bu sekilde gönderiyoruz. tipip ve degisken adi neyse o '<tip:degisken_adi>'
 ]
--- DJANGO/07_DRF_FunctionBasedViews/student_api/views.py
+++ DJANGO/07_DRF_FunctionBasedViews/student_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from rest_framework import status
 #Temel API Görüntüleme
 @api_view()
 def home(request) :
@@ -8,14 +8,58 @@ def home(request) :
         "message" : "Hello-. It is homepage in api!"
     })
 
+# -------------------------------------------------------------------
+'''
+    HTTP Request Types:
+        GET -> Public verilerdir. Listeleme/Görüntüleme işlemlerinde kullanılır.
+        POST -> Private verilerdir. Kayıt oluşturma işlemlerinde kullanılır.
+        * PUT -> Kayıt güncelleme işlemlerinde kullanılır. (Veri bir bütün olarak güncellenir.)
+        * PATCH -> Kayıt güncelleme işlemlerinde kullanılır. (Verinin sadece ilgili kısmı güncellenir.)
+        * DELETE -> Kayıt silme işlemlerinde kullanılır.
+'''
+# -------------------------------------------------------------------
 
+
+#---------------#---------------#---------------#---------------
+#DRF FUNCTION VIEWS
 #---------------#---------------#---------------#---------------
 
 from .serializers import StudentSerializer
 from .models import Student
 
-@api_view()
+@api_view() #Default : ['GET']
 def student_list(request) : 
     students = Student.objects.all()
     serializer = StudentSerializer(students, many=True)
     return Response(serializer.data)
+
+
+#---------------#---------------#---------------#---------------
+#DRF FUNCTION POST - CREATE
+#---------------#---------------#---------------#---------------
+
+@api_view(['POST'])
+def students_create(request) :
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({
+            "message" : "Created Successfully!"
+        },status = status.HTTP_201_CREATED)
+    else : 
+        return Response({
+            "message" : "Data not Validated!"
+        })
+
+
+#---------------#---------------#---------------#---------------
+#DRF FUNCTION PUT
+# StudentSerializer Tek Kayit Görüntüleme
+#---------------#---------------#---------------#---------------
+
+@api_view(['GET'])
+def student_detail(request,pk):
+    student = Student.object.get(id=pk)
+    serializer = StudentSerializer(student)
+    return Response(serializer.data)
+
