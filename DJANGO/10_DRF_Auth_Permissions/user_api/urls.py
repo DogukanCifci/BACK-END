c1d19ebff39ad8ea24abb723e7e13a31c64420a4
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-12-20 12:13:05 +0100

    login-logout method with postman added

--- DJANGO/10_DRF_Auth_Permissions/user_api/urls.py
+++ DJANGO/10_DRF_Auth_Permissions/user_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token # Giris kontrol sistemini yapan hazir fonksiyon
-
+from .views import logout
 
 urlpatterns = [
     path("login/", obtain_auth_token),
+    path("logout/",logout)
 
 ]
--- DJANGO/10_DRF_Auth_Permissions/user_api/views.py
+++ DJANGO/10_DRF_Auth_Permissions/user_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['POST'])
+def logout(request) :
+    
+    if request.method == 'POST' :
+        request.user.auth_token.delete()
+        return Response({
+            "message" : "Token Deleted!"
+        })
+        
